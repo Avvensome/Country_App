58d20606f0454526885a22855be44e24b1f42b3b
Patryk <p.zawistowski190798@gmail.com>
2023-04-17 21:24:20 +0200

    hardcoded-main-country-info

--- country_main.js
+++ country_main.js
@@ -1,10 +1,41 @@
+// Select all elements and inputs
+const input = document.querySelector('.form__input')
+const mainContainer = document.querySelector('main')
+// get value from input
+
+input.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+})
+
+const countryHtmlStructure = (countryInfo) => {
+    `      
+    <div class="container-country-main">
+    <img
+      class="img-country-flag"
+      src="${countryInfo.flags}"
+    />
+    <div class="container-country-lower-section">
+      <div class="country-name-region-container">
+        <h2>${countryInfo.name}</h2>
+        <h3>${countryInfo.region}</h3>
+      </div>
+      <div class="country-info-container">
+        <p>💬 ${countryInfo.languages}</p>
+        <p>💰 ${countryInfo.currencies}</p>
+        <p>👬 ${countryInfo.population / 1000000} M</p>
+      </div>
+    </div>
+  </div>`
+}
+
+
 const getCountry = (countryName) => {
     fetch(`https://restcountries.com/v3.1/name/${countryName}`)
         .then(response => response.json())
         .then(data => {
             console.log(data[0]);
-            console.log(data[0].capital);
         })
 }
 
-// getCountry('poland')
+getCountry('poland')
