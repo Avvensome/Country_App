039072c437f83ab997343d5dd816ff314db6483e
Patryk <p.zawistowski190798@gmail.com>
2023-04-17 23:24:12 +0200

    fully-functional-country-card

--- country_main.js
+++ country_main.js
@@ -3,17 +3,13 @@ const input = document.querySelector('.form__input')
 const mainContainer = document.querySelector('main')
 // get value from input
 
-input.addEventListener('submit', function (e) {
-    e.preventDefault()
-
-})
-
 const countryHtmlStructure = (countryInfo) => {
-    `      
+  return ` 
+
     <div class="container-country-main">
     <img
       class="img-country-flag"
-      src="${countryInfo.flags}"
+      src="${countryInfo.flags.svg}"
     />
     <div class="container-country-lower-section">
       <div class="country-name-region-container">
@@ -21,21 +17,33 @@ const countryHtmlStructure = (countryInfo) => {
         <h3>${countryInfo.region}</h3>
       </div>
       <div class="country-info-container">
-        <p>💬 ${countryInfo.languages}</p>
-        <p>💰 ${countryInfo.currencies}</p>
-        <p>👬 ${countryInfo.population / 1000000} M</p>
-      </div>
-    </div>
-  </div>`
+        <p>💬 ${countryInfo.languages[0].name}</p>
+        <p>💰 ${countryInfo.currencies[0].code}</p>
+  <p>👬 ${(countryInfo.population / 1000000).toFixed(2)} M</p>
+      </div >
+    </div >
+  </div > `
 }
+
 
 
 const getCountry = (countryName) => {
-    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data[0]);
-        })
+  fetch(`https://restcountries.com/v2/name/${countryName}`)
+    .then(response => response.json())
+    .then(data => mainContainer.insertAdjacentHTML('beforeend', countryHtmlStructure(data[0])))
+
 }
 
-getCountry('poland')
+input.addEventListener('keypress', function (e) {
+  if (e.key === "Enter") e.preventDefault()
+  if (e.key === "Enter") {
+    console.log(input.value);
+    getCountry(input.value)
+    input.value = ''
+  }
+})
+
+
+
+
+
